e46ec09802f837895311aaa15c387015373c55a2
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-12-02 11:34:12 -1000

    Add function that makes todo object

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 import "./styles.css";
+
+const makeToDo = () => {
+    if (!localStorage.todo) {
+        localStorage.setItem("todo", "{}");
+        console.log(localStorage.todo);
+    } else {
+        return;
+    }
+};
+
+makeToDo();
+console.log(localStorage);
