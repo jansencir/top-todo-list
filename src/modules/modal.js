34de26b8cf610e19e10a5efa5618ef2d46f0354d
Jansen Rodrigo <169899722+jansencir@users.noreply.github.com>
2024-12-15 03:03:39 -1000

    Create folder to hold modules

---
+++ src/modules/modal.js
@@ -0,0 +1,6 @@
+const addProjectHandler = () => {
+    let addProjectBtn = document.getElementById("add-project-btn");
+    addProjectBtn.addEventListener("click", console.log("clicked"));
+};
+
+export { addProjectHandler };
